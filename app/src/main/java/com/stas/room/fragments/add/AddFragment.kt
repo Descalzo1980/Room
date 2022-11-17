5188cd221b83bc846cf17ec5710ad247752392d6
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-11-17 21:00:01 +0300

    work

--- app/src/main/java/com/stas/room/fragments/add/AddFragment.kt
+++ app/src/main/java/com/stas/room/fragments/add/AddFragment.kt
@@ -1,13 +1,11 @@
 package com.stas.room.fragments.add
 
 import android.os.Bundle
-import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.view.get
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.stas.room.R
@@ -38,20 +36,12 @@ class AddFragment : Fragment() {
     }
 
     private fun insertDataToDataBase() {
-        val firstName = (R.id.et_add_first_name).toString()
-        val secondName = (R.id.et_add_second_name).toString()
-        val age = (R.id.et_add_age)
-
-        if (inputCheck(firstName, secondName, Integer.parseInt(age.toString()))) {
-            val name = Name(0,firstName,secondName, Integer.parseInt(age.toString()))
-            userViewMutableList.addName(name)
-            Toast.makeText(context,"ура!!!!!",Toast.LENGTH_SHORT).show()
-            findNavController().navigate(R.id.action_addFragment_to_listFragment)
-        }else{
-            Toast.makeText(context,"Ну что ты как не родной,",Toast.LENGTH_SHORT).show()
-        }
-    }
-    private fun inputCheck(firstName: String, secondName: String, age: Int) : Boolean {
-        return !(TextUtils.isEmpty(firstName) && TextUtils.isEmpty(secondName) && age != 1)
+        val firstName  = binding.etAddFirstName.text.toString()
+        val secondName = binding.etAddSecondName.text.toString()
+        val age = binding.etAddAge.text
+        val name =  Name(0, firstName, secondName, Integer.parseInt(age.toString()))
+        userViewMutableList.addName(name)
+        Toast.makeText(context, "ура!!!!!", Toast.LENGTH_SHORT).show()
+        findNavController().navigate(R.id.action_addFragment_to_listFragment)
     }
 }
